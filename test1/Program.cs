739798b275224bc5263311b3d84f780596850937
ElhamAghabarari <45203346+ElhamAghabarari@users.noreply.github.com>
2022-10-03 23:30:49 +0330

    all
    first commit

---
+++ test1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+int num = 0;
+while (true)
+{
+    Write("Please enter a number:");
+    if (Int32.TryParse(ReadLine(), out num)) break;
+    WriteLine("error format");
+}
+
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine("{0} {1}",(i % 2==0?"$":"#"),i);
+}
+
